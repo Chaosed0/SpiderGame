02bdd140531790a45757d1c994e2a563cc8cd3b8
chaosed0 <chaosed0@gmail.com>
2016-09-24 19:13:12 -0700

    better

--- premake5.lua
+++ premake5.lua
@@ -10,10 +10,10 @@ project "Engine"
     language "C++"
 	includedirs { "Engine/src/", "Engine/include/" }
     buildoptions "-std=c++14"
-    targetdir "bin/%{cfg.buildcfg}"
+    targetdir "bin/Engine/%{cfg.buildcfg}"
 
     files { "Engine/src/**.h", "Engine/src/**.cpp", "Engine/include/**.h" }
-    links { "BulletDynamics", "BulletCollision", "LinearMath", "SDL2", "SDL2main", "SDL2_image", "OpenAL32", "libsndfile-1", "glew32", "OpenGL32" }
+	defines { "_ITERATOR_DEBUG_LEVEL=0" }
     includedirs {
                     os.getenv("FREETYPE_INCLUDEDIR"),
                     os.getenv("ASSIMP_INCLUDEDIR"),
@@ -29,32 +29,10 @@ project "Engine"
     filter "configurations:Debug"
         defines { "DEBUG" }
         debugdir "./"
-        libdirs {
-            os.getenv("FREETYPE_LIBDIR_D"),
-            os.getenv("ASSIMP_LIBDIR_D"),
-            os.getenv("BULLET_LIBDIR_D"),
-            os.getenv("SDL_LIBDIR_D"),
-            os.getenv("SDL_IMAGE_LIBDIR_D"),
-            os.getenv("OPENAL_LIBDIR_D"),
-            os.getenv("LIBSNDFILE_LIBDIR_D"),
-            os.getenv("GLEW_LIBDIR_D"),
-        }
-        links { "freetype263d", "assimpd" }
         flags { "Symbols" }
 
     filter "configurations:Release"
         debugdir "./"
-        libdirs {
-            os.getenv("FREETYPE_LIBDIR"),
-            os.getenv("ASSIMP_LIBDIR"),
-            os.getenv("BULLET_LIBDIR"),
-            os.getenv("SDL_LIBDIR"),
-            os.getenv("SDL_IMAGE_LIBDIR"),
-            os.getenv("OPENAL_LIBDIR"),
-            os.getenv("LIBSNDFILE_LIBDIR"),
-            os.getenv("GLEW_LIBDIR"),
-        }
-        links { "freetype263", "assimp" }
         optimize "On"
 
 
@@ -62,12 +40,13 @@ project "SpiderGame"
     kind "ConsoleApp"
 	location "build"
     language "C++"
-	includedirs { "src/SpiderGame/", "Engine/include/" }
+	includedirs { "SpiderGame/src/", "Engine/include/" }
     buildoptions "-std=c++14"
-    targetdir "bin/%{cfg.buildcfg}"
+    targetdir "bin/Game/%{cfg.buildcfg}"
 
     files { "SpiderGame/src/**.h", "SpiderGame/src/**.cpp" }
-    links { "BulletDynamics", "BulletCollision", "LinearMath", "SDL2", "SDL2main", "SDL2_image", "libnoise", "Engine" }
+	links { "BulletDynamics", "BulletCollision", "LinearMath", "SDL2", "SDL2main", "SDL2_image", "OpenAL32", "libsndfile-1", "glew32", "OpenGL32", "libnoise", "Engine" }
+	defines { "_ITERATOR_DEBUG_LEVEL=0" }
     includedirs {
                     os.getenv("BULLET_INCLUDEDIR"),
 					os.getenv("GLM_INCLUDEDIR"),
@@ -81,23 +60,35 @@ project "SpiderGame"
         defines { "DEBUG" }
         debugdir "./"
         libdirs {
-            os.getenv("SDL_LIBDIR_D"),
-            os.getenv("SDL_IMAGE_LIBDIR_D"),
-            os.getenv("BULLET_LIBDIR_D"),
+			    os.getenv("FREETYPE_LIBDIR_D"),
+				os.getenv("ASSIMP_LIBDIR_D"),
+				os.getenv("BULLET_LIBDIR_D"),
+				os.getenv("SDL_LIBDIR_D"),
+				os.getenv("SDL_IMAGE_LIBDIR_D"),
+				os.getenv("OPENAL_LIBDIR_D"),
+				os.getenv("LIBSNDFILE_LIBDIR_D"),
+				os.getenv("GLEW_LIBDIR_D"),
             os.getenv("NOISE_LIBDIR_D"),
             os.getenv("EXTRA_LIBDIR_D")
         }
+		links { "freetype263d", "assimpd" }
         flags { "Symbols" }
 
     filter "configurations:Release"
         debugdir "./"
         libdirs {
-            os.getenv("SDL_LIBDIR"),
-            os.getenv("SDL_IMAGE_LIBDIR"),
-            os.getenv("BULLET_LIBDIR"),
+				os.getenv("FREETYPE_LIBDIR"),
+				os.getenv("ASSIMP_LIBDIR"),
+				os.getenv("BULLET_LIBDIR"),
+				os.getenv("SDL_LIBDIR"),
+				os.getenv("SDL_IMAGE_LIBDIR"),
+				os.getenv("OPENAL_LIBDIR"),
+				os.getenv("LIBSNDFILE_LIBDIR"),
+				os.getenv("GLEW_LIBDIR"),
             os.getenv("NOISE_LIBDIR"),
             os.getenv("EXTRA_LIBDIR")
         }
+		links { "freetype263", "assimp" }
         optimize "On"
 		
     filter "files:assets"
